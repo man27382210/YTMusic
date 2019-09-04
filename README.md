f5079cf12c7319de3dc9d1ede2c3e632c4a82301
twtseng <twtseng@indeed.com>
2019-09-04 16:10:59 +0900

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+#YTMusic
+
+Base on [electron-react-boilerplate](https://github.com/electron-react-boilerplate/electron-react-boilerplate)
---
+++ app/containers/App.js
@@ -0,0 +1,11 @@
+// @flow
+import * as React from 'react';
+
+export default () => (
+  <div style={{ height: '100%' }}>
+    <webview
+      src="https://music.youtube.com"
+      style={{ width: 'auto', height: '100%' }}
+    />
+  </div>
+);
